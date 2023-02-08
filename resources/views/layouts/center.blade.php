b501279daaa464c5e08a35056b25fb1c251acd18
Vasili Yurevich <vasyayurevich@gmail.com>
2023-02-08 22:38:22 +0300

    init project v1

---
+++ resources/views/layouts/center.blade.php
@@ -0,0 +1,230 @@
+<section class="py-15 py-xl-20 ">
+    <style>
+        .bg6{
+            float: right;
+            width: 500px;
+            display: flex;
+        }
+    </style>
+    <img class="bg6" src="{{Vite::asset('resources/images/bg6.png')}}" alt="">
+    <div class="container mt-10 level-3">
+
+        <div class="row mb-md-10">
+            <div class="col-md-10 col-lg-8">
+                <h1 class="display-2 font-weight-bold">Global Stake</h1>
+            </div>
+        </div>
+        <div class="row g-3 g-xl-5">
+            <div class="col-md-6 col-lg-4">
+                <span class="h3 fw-bold">01.</span>
+                <h4 class="h5 my-2">Cryptocurrency</h4>
+                <p class="text-muted">This is becoming the "New Money" in the finance industry, making waves and amazing returns on invested capital.</p>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <span class="h3 fw-bold">02.</span>
+                <h4 class="h5 my-2">Stocks & Bonds</h4>
+                <p class="text-muted">
+                    Stocks & Bonds
+                    These markets allow sales and purchases of stocks and other financial products such as commodities and futures.</p>
+            </div>
+            <div class="col-lg-4">
+                <span class="h3 fw-bold">03.</span>
+                <h4 class="h5 my-2">Forex</h4>
+                <p class="text-muted">With the Meta Trader 5 trading software, this sector has remained innovative and profitable over the years.</p>
+            </div>
+        </div>
+    </div>
+
+{{--    <figure class="background background-overlay" style="background-image: url('https://www.paymentsjournal.com/wp-content/uploads/2022/04/1661-scaled-e1649434277532-750x375.jpg')">--}}
+{{--    <figure class="background background-overlay"--}}
+{{--            style="background-image: url({{Vite::asset('resources/images/bg6.png')}}"></figure>--}}
+</section>
+
+
+<div class="py-15 py-xl-20"  data-edge-strategy="reset" style="">
+    <span class="bg-color bg-light"></span>
+
+    <section class="mb-10 aos-init" data-aos="fade-up" data-aos-delay="150">
+        <div class="container block-2">
+            <div class="row g-0">
+                <div class="col-lg-6 order-lg-2 position-relative">
+                    <style>
+                        .block-2{
+                            padding: 3%;
+                            background: #fff;
+                            color:  rgb(6, 17, 33);
+                            border-radius: 50px;
+                        }
+                    </style>
+                    <span class="background" style=" border-radius: 50px;background-image: url('{{Vite::asset('resources/images/2.png')}}')"></span>
+                </div>
+                <div class="col-lg-6 p-4 p-md-10 order-lg-1">
+                    <h3 class="fw-bold">What We Do</h3>
+                    <p class="fs-lg text-secondary">We are engaged with leading Bitcoin mining and crypto currency trading. It has no doubt that market of Bitcoin is incredibly increasing, So we offer our customers different and suitable investment plans tailored to meet the needs of both small and big investors.</p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <ul class="list-group list-group-minimal">
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Brand Strategy
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Visual Identity
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Rebranding
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="list-group list-group-minimal">
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Brand
+                                    Strategy
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Visual Identity
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Rebranding
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb-10 aos-init aos-animate" data-aos="fade-up" data-aos-delay="150">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-lg-6 p-4 p-md-10 order-lg-2">
+                    <h3 class="fw-bold">Benefits of Using <span>Our Solution</span></h3>
+                    <p class="fs-lg text-secondary">The benefits of investing with Global Stake include the ability to gain exposure to different asset classes and market sectors.</p>
+                    <div class="row" style="
+    white-space: nowrap;
+">
+                        <div class="col-md-6">
+                            <ul class="list-group list-group-minimal">
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Diversification
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Potential earnings
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Cost-effective investment
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="list-group list-group-minimal">
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Access to a range of assets
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Geared exposure
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Well managed funds
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-lg-1 position-relative">
+                    <div class="diamond-icon">
+                        <img src="{{Vite::asset('resources/images/1.png')}}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section data-aos="fade-up" data-aos-delay="150" class="aos-init">
+        <div class="container block-2">
+            <div class="row g-0">
+                <div class="col-lg-6 order-lg-2 position-relative">
+                    <span class="background" style=" border-radius: 50px;background-image: url('{{Vite::asset('resources/images/3.png')}}')"></span>
+                </div>
+                <div class="col-lg-6 p-4 p-md-10 order-lg-2">
+                    <h3 class="fw-bold">User Experience</h3>
+                    <p class="fs-lg text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing, elit. Voluptatum
+                        impedit
+                        sunt, quam, fugit
+                        inventore repellat odit, vero aliquid odio magnam at molestiae dolor sequi pariatur quasi,
+                        voluptates
+                        similique velit consequatur.</p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <ul class="list-group list-group-minimal">
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Brand Strategy
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Visual Identity
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Rebranding
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="list-group list-group-minimal">
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Brand
+                                    Strategy
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Visual Identity
+                                </li>
+                                <li class="list-group-item">
+                                    <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-1"><i
+                                            class="bi bi-check2 text-primary"></i></div>
+                                    Rebranding
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
